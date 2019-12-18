90b9520ce1d6ca6fc0574a43ec0c017de36f1188
sasha-go <sasha.lfg@gmail.com>
2019-12-17 22:11:54 -0800

    added STORE, js, and made edits to css/html

---
+++ STORE.js
@@ -0,0 +1,109 @@
+const STORE = [
+    {
+      question: 'What auspicious day was Fred and George Weasley born?',
+      answers: [
+        'February 3rd, 1978',
+        'April 1st, 1978',
+        'October 23rd, 1977',
+        'October 31st, 1978'],
+      correctAnswer: 'April 1st, 1978'
+    },
+  
+    {
+      question: 'In the book Harry Potter and the Goblet of Fire, how did Harry get the idea of using Gillyweed for the second task of the Tri-Wizard Tournament?',
+      answers: [
+        'Neville goes to Harry and gives him the idea of using Gillyweed after reading Magical Water Plants of the Mediterranean',
+        'Dobby steals Gillyweed from Professor Snape and gives it Harry after overhearing a conversation between ‘Professor Moody’ and Professor McGonagall about its uses',
+        'Harry reads the Magical Water Plants of the Mediterranean and obtains Gillyweed from Professor Sprout',
+        'Cedric Diggory tips off Harry about Gillyweed after Harry helped him with the first task'],
+      correctAnswer: 'Dobby steals Gillyweed from Professor Snape and gives it Harry after overhearing a conversation between ‘Professor Moody’ and Professor McGonagall about its uses'
+    },
+    
+    {
+      question: 'What is the name of the all-female Quidditch team founded in 1203?',
+      answers: [
+        'Appleby Arrows',
+        'Holyhead Harpies',
+        'Montrose Magpies',
+        'Bulgarian National Quidditch Team'],
+      correctAnswer: 'Holyhead Harpies'
+    },
+    
+    {
+      question: 'What was Nymphadora Tonks’ original patronus before it changed into a wolf?',
+      answers: [
+        'Jack Rabbit',
+        'Kangaroo',
+        'Fennec Fox',
+        'Doe'
+      ],
+      correctAnswer: 'Jack Rabbit'
+    },
+    
+    {
+      question: 'Both of Hermione’s parents had what occupation in the Muggle World?',
+      answers: [
+        'Family Law Lawyers',
+        'Dentists',
+        'Anesthesiologists',
+        'Mail Carriers'
+      ],
+      correctAnswer: 'Dentists'
+    },
+    
+    {
+      question: 'Which Harry Potter film(s) did Peeves the Poltergeist make an appearance?',
+      answers: [
+        'The Sorcerer’s Stone and The Chamber of Secrets',
+        'The Sorcerer’s Stone',
+        'The Prisoner of Azkaban',
+        'None'
+      ],
+      correctAnswer: 'None'
+    },
+    
+    {
+      question: 'Who is the creator of the Sectumsempra spell?',
+      answers: [
+        'Tom Riddle',
+        'Severus Snape',
+        'James Potter',
+        'Bellatrix LeStrange'
+      ],
+      correctAnswer: 'Severus Snape
+    },
+    
+    {
+      question: 'Which Weasley sibling never had an acting scene in the Harry Potter films?',
+      answers: [
+        'Charlie Weasley',
+        'Molly Weasley',
+        'Bill Weasley',
+        'Percy Weasley'
+      ],
+      correctAnswer: 'Charlie Weasley'
+    },
+    
+    {
+      question: 'In the Goblet of Fire, Hermione founded an organization named S.P.E.W in response to the injustices she saw in the treatment of house-elves. What does S.P.E.W stand for?',
+      answers: [
+        'Society for Protecting Elves at Work',
+        'Spell Protection for Elves working for Wizards',
+        'Society for the Promotion of Elfish Welfare',
+        'Super Powered Elves at Work'],
+      correctAnswer: 'Society for the Promotion of Elfish Welfare'
+    },
+    
+    {
+      question: 'What does Dumbledore says he sees when he looks into the Mirror of Erised?',
+      answers: [
+        'He sees himself holding a pair of thick, woolen socks',
+        'He sees himself with his siblings, Ariana and Aberforth',
+        'He sees himself winning the battle against Voldemort',
+        'He sees himself eating a Bertie Bott’s bean that is not booger flavored'
+      ],
+      correctAnswer: 'He sees himself holding a pair of thick, woolen socks'
+    },
+    
+    
+    ];
---
+++ script.js
@@ -0,0 +1,32 @@
+
+function startQuiz() {
+    ${'#startButton'}.on('click', function(e){
+      renderAQuestion();
+    });
+  }
+  
+  function updateQuestionAndScore() {
+   //create a const html to increment the question by 1 when answering a question / questions.length
+   // score / questions.length
+  }
+  
+  function updateAnswerOptions() {
+  
+  }
+  
+  function renderAQuestion() {
+  
+  }
+  
+  function displayResults() {
+    
+  }
+  
+  
+  function restartQuiz() {
+    ${'#restartButton'}.on('click', function(e) {
+      renderAQuestion();
+    });
+  }
+  
+  //Don't forget to add handleApp function here!
